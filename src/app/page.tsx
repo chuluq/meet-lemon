ba352a06f584a77fc6ec739cf66ded12fe50af35
chuluq <chuluq@arvis.id>
2024-11-11 11:30:42 +0700

    style: add selection text color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { Hero } from "@/components/hero";
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-white selection:bg-[#EBFA9E]">
       <Header />
       <Hero />
-    </>
+    </div>
   );
 }
