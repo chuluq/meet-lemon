32ba4ea5dc977333682e8ff5b7ffa23471624e43
chuluq <chuluq@arvis.id>
2024-11-11 13:41:05 +0700

    feat: add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Banner } from "@/components/banner";
 import { Header } from "@/components/header";
 import { Hero } from "@/components/hero";
 
 export default function Home() {
   return (
     <div className="bg-white selection:bg-[#EBFA9E]">
+      <Banner />
       <Header />
       <Hero />
     </div>
--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,3 +1,7 @@
 export const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="bg-[#2A514C] py-[18px] text-center font-sans text-sm text-white">
+      Use this banner to inform your users of something important.
+    </div>
+  );
 };
